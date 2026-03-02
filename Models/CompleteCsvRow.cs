cb36b3db9d89a898dd97328ec4a967bfc3da5341
ALP <a.b.lindgard@gmail.com>
2026-03-02 14:24:24 +0100

    started filling out the models with what I think they'll need from the files

--- Models/CompleteCsvRow.cs
+++ Models/CompleteCsvRow.cs
@@ -1,6 +1,18 @@
+using Microsoft.VisualBasic;
+
 namespace be_linqBasics.Models;
 
 public class CompleteCsvRow
 {
-
+    public string City { get; set; }
+    public int Year { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string Shape { get; set; }
+    public int DurationInSeconds { get; set; }
+    public string Duration { get; set; }
+    public string Comments { get; set; }
+    public int DatePosted { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
 }
--- Models/ScrubbedCsvRow.cs
+++ Models/ScrubbedCsvRow.cs
@@ -2,5 +2,15 @@ namespace be_linqBasics.Models;
 
 public class ScrubbedCsvRow
 {
-
+    public string City { get; set; }
+    public int Year { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+    public string Shape { get; set; }
+    public int DurationInSeconds { get; set; }
+    public string Duration { get; set; }
+    public string Comments { get; set; }
+    public int DatePosted { get; set; }
+    public decimal Latitude { get; set; }
+    public decimal Longitude { get; set; }
 }
