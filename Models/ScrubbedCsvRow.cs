cf4ac00bc663afbf475f8f3f0d0836e8976774ed
ALP <a.b.lindgard@gmail.com>
2026-03-02 14:39:12 +0100

    added default value to strings

--- Models/ScrubbedCsvRow.cs
+++ Models/ScrubbedCsvRow.cs
@@ -2,14 +2,14 @@ namespace be_linqBasics.Models;
 
 public class ScrubbedCsvRow
 {
-    public string City { get; set; }
+    public string City { get; set; } = string.Empty;
     public int Year { get; set; }
-    public string State { get; set; }
-    public string Country { get; set; }
-    public string Shape { get; set; }
+    public string State { get; set; } = string.Empty;
+    public string Country { get; set; } = string.Empty;
+    public string Shape { get; set; } = string.Empty;
     public int DurationInSeconds { get; set; }
-    public string Duration { get; set; }
-    public string Comments { get; set; }
+    public string Duration { get; set; } = string.Empty;
+    public string Comments { get; set; } = string.Empty;
     public int DatePosted { get; set; }
     public decimal Latitude { get; set; }
     public decimal Longitude { get; set; }
