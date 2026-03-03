1d3efbcbfd00fae3320d631ed442ab2be95db0b5
ALP <a.b.lindgard@gmail.com>
2026-03-03 10:48:52 +0100

    trying to get a little bit of logic in, make use of the model I've built and the file in Data.

--- Views/CsvReader.cs
+++ Views/CsvReader.cs
@@ -1,6 +1,11 @@
+using be_linqBasics.Models;
+
 namespace be_linqBasics.Views;
 
 public class CsvReader
 {
-
+    public List<ScrubbedCsvRow> /*Read("./Data/scrubbed.csv")*/
+    {
+        var lines = File.ReadAllLines("./Data/scrubbed.csv");
+    }
 }
