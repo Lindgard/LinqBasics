05ed8deb67471d078bc62bd03c4e0a4f459ce48e
ALP <a.b.lindgard@gmail.com>
2026-06-18 10:14:28 +0200

    wrote a constructor and started the Run method that will handle linq queries from LinqService file

--- Controllers/UfoController.cs
+++ Controllers/UfoController.cs
@@ -5,6 +5,28 @@ namespace be_linqBasics.Controllers;
 
 public class UfoController
 {
-    private readonly LinqService _service;
-    private readonly CsvReader _view;
+    private readonly LinqServices _service;
+
+    /// <summary>
+    /// Constructor for the UfoController class. It initializes the LinqServices with the sightings read from the CSV file.
+    /// </summary>
+    /// <param name="csvPath">The path to the CSV file containing the sightings data.</param>
+    public UfoController(string csvPath = "Data/scrubbed.csv")
+    {
+        var csvReader = new CsvReader();
+        var sightings = csvReader.ReadScrubbed();
+        _service = new LinqServices(sightings);
+
+        Console.WriteLine($"Loaded {sightings.Count} sightings from the '{csvPath}' file.\n");
+    }
+
+    /// <summary>
+    /// Runs the LINQ queries and displays the results. 
+    /// This method can be expanded to include calls to the various LINQ query methods 
+    /// in the LinqServices class and display their results.
+    /// </summary>
+    public void Run()
+    {
+
+    }
 }
